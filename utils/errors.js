b92b6af5144b7bccca126016d530598d2def3dde
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2022-11-18 19:52:01 +0100

    delete comments

--- utils/errors.js
+++ utils/errors.js
@@ -44,17 +44,3 @@ module.exports = {
   notFoundError,
   defaultError,
 };
-
-// const errorHandling = (err, res) => {
-//   if (err.name === 'ValidationError' || err.name === 'CastError') {
-//     res.status(400).send({ message: err.message });
-
-//     return;
-//   }
-//   if (err.statusCode === 404) {
-//     res.status(err.statusCode).send({ message: err.message });
-
-//     return;
-//   }
-//   res.status(500).send({ message: err.message });
-// };

d66dc48c6c7df02d7f7e4cde721da3fbf7b8841f
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2022-11-20 23:51:57 +0100

    step 7

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
-const { getUser, getUsers, createUser } = require('../controllers/users');
+const { getUser } = require('../controllers/users');
+
+const getCurrentUser = (req, res) => {
+  getUser(req, res);
+};
+
+router.get('/me', getCurrentUser);
+router.patch('/me', getCurrentUser);
 
 module.exports = router;

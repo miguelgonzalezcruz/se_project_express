35b40eb40dd7bdee0af3ad71a8678569f536571e
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2022-11-22 21:04:10 +0100

    reorder signin signup

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ mongoose.connect('mongodb://localhost:27017/wtwr_db');
 
 const app = express();
 
+app.post('/signin', login);
+app.post('/signup', createUser);
+
 app.use(auth);
 
 app.use(express.json());
@@ -18,9 +21,6 @@ app.use('/items', require('./routes/clothingItems'));
 app.use((req, res) => {
   res.status(404).send({ message: 'Requested resource not found' });
 });
-
-app.post('/signin', login);
-app.post('/signup', createUser);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);

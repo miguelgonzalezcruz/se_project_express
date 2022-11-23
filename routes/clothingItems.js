cde02825f26946c2d035c948cb63f1d3161906ec
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2022-11-23 18:31:21 +0100

    fix clothing routes

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
-const { auth } = require('../middlewares/auth');
+
+const { getItems } = require('../controllers/clothingItems');
 const {
-  getItems,
   createItem,
   deleteItem,
   likeItem,
   dislikeItem,
 } = require('../controllers/clothingItems');
+const auth = require('../middlewares/auth');
 
 router.get('/', getItems);
 router.post('/', auth, createItem);

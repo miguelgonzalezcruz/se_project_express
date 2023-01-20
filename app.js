4d4084d90549139971717a9e489c376af5e0b3bb
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-01-20 17:12:15 +0100

    add Cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
+
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
@@ -20,6 +22,19 @@ const allowedOrigins = [
 ];
 
 app.use(cors({ origin: allowedOrigins }));
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

081f54e7b047a789b6feae621f3e089b5a0a8cb4
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-01-24 21:50:21 +0100

    again with helmet and limiter

--- app.js
+++ app.js
@@ -5,15 +5,15 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-// const helmet = require('helmet');
-// const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-//   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-//   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-// });
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandling = require('./middlewares/errorHandling');
@@ -24,6 +24,8 @@ const { PORT = 3001 } = process.env;
 mongoose.connect('mongodb://localhost:27017/wtwr_db');
 
 const app = express();
+app.use(limiter);
+app.use(helmet());
 
 const allowedOrigins = [
   'https://wtwrmgc.students.nomoredomainssbs.ru',
@@ -32,10 +34,6 @@ const allowedOrigins = [
 ];
 
 app.use(cors({ origin: allowedOrigins }));
-
-// app.use(limiter);
-
-// app.use(helmet());
 
 app.use((req, res, next) => {
   const { origin } = req.headers;

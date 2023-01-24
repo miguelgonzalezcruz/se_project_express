e9ab0de45300bc4b458ed0cda6a081902490ae20
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-01-24 21:48:09 +0100

    fix auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../utils/config');
-const { UnauthorizedError } = require('../errors/unauthorized-error');
+const UnauthorizedError = require('../errors/unauthorized-error');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;

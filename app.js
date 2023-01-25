bdcbcedf7b1d715247f2be1f6d5bfb1b07a0f50d
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-01-25 20:53:09 +0100

    helmet and limiter again

--- app.js
+++ app.js
@@ -5,6 +5,15 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 const { celebrate, Joi } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -37,6 +46,9 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.use(limiter);
+app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
